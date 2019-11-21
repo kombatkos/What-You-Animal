e1ab57f506dddb2ad95a9025ecbc4a191e9fd9aa
Ivan <kombat_kos@mail.ru>
2019-11-21 12:07:29 +0400

    Initial Commit

---
+++ What You Animal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What You Animal
+//
+//  Created by kot on 21/11/2019.
+//  Copyright © 2019 Konstantin Porokhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
